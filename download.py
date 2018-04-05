33a34ec75eba47471d567bcf1672098ba3d5f006
Michael Snider <snidemic@gmail.com>
2018-04-04 19:10:48 -0700

    Add basic download implementation
    - Update `download` to parse `uri`, `provider`, `username`,
      and `outputDir` request parameters.
    
    - Implement video downloading and transcoding using `youtube_dl` module,
      keeping the request open until both operations are complete.

--- download.py
+++ download.py
@@ -1,8 +1,27 @@
-import pprint
-from flask import Flask, request
+from __future__ import unicode_literals
+import youtube_dl
+import os
+from flask import abort, Flask, request
 app = Flask(__name__)
 
-pp = pprint.PrettyPrinter(indent=4)
+output_format = '%(title)s.%(ext)s'
+
+class ErrorLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+ydl_opts = {
+    'download_archive': 'downloads.log',
+    'format': 'best',
+    'logger': ErrorLogger(),
+    'progress_hooks': [],
+}
 
 @app.route('/', methods=['GET'])
 def help():
@@ -15,4 +34,28 @@ def help():
 
 @app.route('/', methods=['POST'])
 def download():
-    return pp.pformat(request.form)
+    url = request.form['url']
+    opts = ydl_opts
+
+    # add authentication parameters if present in request
+
+    if 'provider' in request.form:
+        # TODO: setup proper provider -> ap_mso mapping
+        opts['ap_mso'] = 'Comcast_SSO'
+        print('Setting up authentication using {}'.format(opts['ap_mso']))
+        opts['ap_username'] = request.form['username']
+        opts['ap_password'] = request.form['password']
+
+    output_dir = os.getcwd()
+    if 'outputDir' in request.form:
+        base_dir = os.environ['BASE_DIR']
+        output_dir = '{}/{}'.format(base_dir, request.form['outputDir'])
+
+    opts['outtmpl'] = output_dir + '/' + output_format
+
+    # download video
+    with youtube_dl.YoutubeDL(opts) as ydl:
+        if ydl.download([url]) != 0:
+            abort(500)
+
+    return "Successfully downloaded to {}".format(output_dir)
