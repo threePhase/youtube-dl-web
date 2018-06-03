6a52c0f015dc1fae90b7a9e3484b9fc6b383c9a8
Michael Snider <snidemic@gmail.com>
2018-06-03 10:23:15 -0700

    Load top-level config before instance and test config

--- ytdlw/__init__.py
+++ ytdlw/__init__.py
@@ -5,6 +5,9 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True,
         subdomain_matching=True)
 
+    # load default configuration
+    app.config.from_object('config')
+
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
